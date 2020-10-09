ef9bb0a71a9eb583bd10e99b5a9a3c1a3efdf6d8
Jeremy Sapsed <jeremy@sapsed.co.uk>
2020-10-09 20:23:55 +0100

    Added historic versions

---
+++ Old Versions/v1.0/PhotoTagger_Classic/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace PhotoTagger_Classic
+{
+    partial class PhotoTaggerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhotoTaggerForm));
+            this.pickFolderButton = new System.Windows.Forms.Button();
+            this.foldersLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.tagButton = new System.Windows.Forms.Button();
+            this.cancelTagButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.folderListText = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.openFolderCheckBox = new System.Windows.Forms.CheckBox();
+            this.LogoPreview = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pickFolderButton
+            // 
+            this.pickFolderButton.Location = new System.Drawing.Point(12, 12);
+            this.pickFolderButton.Name = "pickFolderButton";
+            this.pickFolderButton.Size = new System.Drawing.Size(75, 23);
+            this.pickFolderButton.TabIndex = 0;
+            this.pickFolderButton.Text = "Pick Folder";
+            this.pickFolderButton.UseVisualStyleBackColor = true;
+            this.pickFolderButton.Click += new System.EventHandler(this.PickFolderButton_Click);
+            // 
+            // foldersLabel
+            // 
+            this.foldersLabel.AutoSize = true;
+            this.foldersLabel.Location = new System.Drawing.Point(12, 74);
+            this.foldersLabel.Name = "foldersLabel";
+            this.foldersLabel.Size = new System.Drawing.Size(89, 13);
+            this.foldersLabel.TabIndex = 1;
+            this.foldersLabel.Text = "Folders Selected:";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(93, 12);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(77, 23);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear Folders";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // tagButton
+            // 
+            this.tagButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tagButton.Location = new System.Drawing.Point(397, 232);
+            this.tagButton.Name = "tagButton";
+            this.tagButton.Size = new System.Drawing.Size(75, 23);
+            this.tagButton.TabIndex = 4;
+            this.tagButton.Text = "Tag Images";
+            this.tagButton.UseVisualStyleBackColor = true;
+            this.tagButton.Click += new System.EventHandler(this.tagButton_Click);
+            // 
+            // cancelTagButton
+            // 
+            this.cancelTagButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cancelTagButton.Enabled = false;
+            this.cancelTagButton.Location = new System.Drawing.Point(12, 232);
+            this.cancelTagButton.Name = "cancelTagButton";
+            this.cancelTagButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelTagButton.TabIndex = 6;
+            this.cancelTagButton.Text = "Cancel";
+            this.cancelTagButton.UseVisualStyleBackColor = true;
+            this.cancelTagButton.Click += new System.EventHandler(this.cancelTagButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.folderListText);
+            this.panel1.Location = new System.Drawing.Point(12, 90);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(460, 136);
+            this.panel1.TabIndex = 7;
+            // 
+            // folderListText
+            // 
+            this.folderListText.AutoSize = true;
+            this.folderListText.Location = new System.Drawing.Point(4, 4);
+            this.folderListText.Name = "folderListText";
+            this.folderListText.Size = new System.Drawing.Size(0, 13);
+            this.folderListText.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(93, 232);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(298, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // openFolderCheckBox
+            // 
+            this.openFolderCheckBox.AutoSize = true;
+            this.openFolderCheckBox.Checked = true;
+            this.openFolderCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.openFolderCheckBox.Location = new System.Drawing.Point(12, 41);
+            this.openFolderCheckBox.Name = "openFolderCheckBox";
+            this.openFolderCheckBox.Size = new System.Drawing.Size(142, 17);
+            this.openFolderCheckBox.TabIndex = 9;
+            this.openFolderCheckBox.Text = "Open folders when done";
+            this.openFolderCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // LogoPreview
+            // 
+            this.LogoPreview.ImageLocation = "logo.png";
+            this.LogoPreview.Location = new System.Drawing.Point(177, 28);
+            this.LogoPreview.Name = "LogoPreview";
+            this.LogoPreview.Size = new System.Drawing.Size(295, 46);
+            this.LogoPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.LogoPreview.TabIndex = 10;
+            this.LogoPreview.TabStop = false;
+            this.LogoPreview.Click += new System.EventHandler(this.LogoPreview_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Logo Preview (Click to change):";
+            // 
+            // PhotoTaggerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(484, 265);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LogoPreview);
+            this.Controls.Add(this.openFolderCheckBox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cancelTagButton);
+            this.Controls.Add(this.tagButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.foldersLabel);
+            this.Controls.Add(this.pickFolderButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(300, 200);
+            this.Name = "PhotoTaggerForm";
+            this.Text = "Photo Tagger";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pickFolderButton;
+        private System.Windows.Forms.Label foldersLabel;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button tagButton;
+        private System.Windows.Forms.Button cancelTagButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label folderListText;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.CheckBox openFolderCheckBox;
+        private System.Windows.Forms.PictureBox LogoPreview;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Old Versions/v1.0/PhotoTagger_Classic/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoTagger_Classic
+{
+    public partial class PhotoTaggerForm : Form
+    {
+
+        private IList<String> folderPaths = new List<String>();
+        private IList<String> images = new List<String>();
+        private String logoPath;
+
+        public PhotoTaggerForm()
+        {
+            InitializeComponent();
+            updateList();
+            logoPath = "logo.png";
+        }
+
+        private void PickFolderButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderPaths.Add(dialog.SelectedPath);
+
+                updateList();
+            }
+        }
+
+        private IList<String> getImagesInFolders(IList<String> folders)
+        {
+            IList<String> images = new List<String>();
+            folderListText.Text = "";
+            foreach (String folderPath in folders)
+            {
+                images = addList(images, getImagesInFolder(folderPath));
+
+            }
+            return images;
+        }
+
+        private IList<String> addList(IList<String> list1, IList<String> list2)
+        {
+            foreach (String item in list2)
+            {
+                list1.Add(item);
+            }
+            return list1;
+        }
+
+        private IList<String> getImagesInFolder(String folderPath)
+        {
+            IList<String> imagePaths = new List<String>();
+
+            var jpgs = Directory.EnumerateFiles(folderPath, "*.jpg");
+            var jpegs = Directory.EnumerateFiles(folderPath, "*.jpeg");
+            var pngs = Directory.EnumerateFiles(folderPath, "*.png");
+
+            foreach (string filePath in jpgs)
+            {
+                imagePaths.Add(filePath);
+            }
+            foreach (String filePath in jpegs)
+            {
+                imagePaths.Add(filePath);
+            }
+            foreach (String filePath in pngs)
+            {
+                imagePaths.Add(filePath);
+            }
+            
+            return imagePaths;
+        }
+
+        private void updateList()
+        {
+            if (folderPaths.Count > 0)
+            {
+                clearButton.Enabled = true;
+                tagButton.Enabled = true;
+
+                folderListText.Text = "";
+
+                foreach (String folder in folderPaths)
+                {
+                    folderListText.Text += (folder + "\n");
+
+                }
+            }
+            else
+            {
+                clearButton.Enabled = false;
+                tagButton.Enabled = false;
+                folderListText.Text = "No folders selected...";
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            folderPaths.Clear();
+            updateList();
+        }
+
+        private Image applyEXIFRotation(Image image)
+        {
+            if (Array.IndexOf(image.PropertyIdList, 274) > 1)
+            {
+                var orientation = (int)image.GetPropertyItem(274).Value[0];
+
+                switch (orientation)
+                {
+                    case 1:
+                        // Not rotation required
+                        break;
+                    case 2:
+                        image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                        break;
+                    case 3:
+                        image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        break;
+                    case 4:
+                        image.RotateFlip(RotateFlipType.Rotate180FlipX);
+                        break;
+                    case 5:
+                        image.RotateFlip(RotateFlipType.Rotate90FlipX);
+                        break;
+                    case 6:
+                        image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        break;
+                    case 7:
+                        image.RotateFlip(RotateFlipType.Rotate270FlipX);
+                        break;
+                    case 8:
+                        image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        break;
+                }
+                image.RemovePropertyItem(274);
+            }
+            return image;
+        }
+
+        private Bitmap applyEXIFData(Image inputImage, Bitmap outputImage)
+        {
+            foreach (var id in inputImage.PropertyIdList)
+            {
+                outputImage.SetPropertyItem(inputImage.GetPropertyItem(id));
+            }
+            
+            return outputImage;
+        }
+
+        private Bitmap tagImage(Image imageToTag)
+        {
+            Image logo = Image.FromFile(logoPath);
+            imageToTag = applyEXIFRotation(imageToTag);
+            double scaleFactor = 1;
+            double ratio = .2;
+            if (imageToTag.Width <= imageToTag.Height)
+            {
+                ratio = .4;
+            }
+            int logoW = (int)((double)imageToTag.Width * ratio);
+            scaleFactor = (double)logoW / (double)logo.Width;
+
+            Bitmap logoScaled = ResizeImage(logo, scaleFactor);
+
+            Bitmap outputImage = new Bitmap(imageToTag);
+            outputImage = applyEXIFData(imageToTag, outputImage);
+
+            using (Graphics graphics = Graphics.FromImage(outputImage))
+            {
+                graphics.DrawImage(logoScaled, new Point(0, imageToTag.Height - logoScaled.Height));
+            }
+            logo.Dispose();
+            logoScaled.Dispose();
+            imageToTag.Dispose();
+            return outputImage;
+        }
+
+        private Bitmap ResizeImage(Image image, double factor)
+        {
+            int width = (int)(image.Width * factor);
+            int height = (int)(image.Height * factor);
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+
+                graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel);
+            }
+            image.Dispose();
+            
+            return destImage;
+        }
+
+        private String getNewFileName(String fileName)
+        {
+            String newDirectory = fileName.Substring(0, fileName.LastIndexOf('\\') + 1) + "Tagged";
+            String newFileName = newDirectory + fileName.Substring(fileName.LastIndexOf('\\'));
+
+            bool exists = System.IO.Directory.Exists(newDirectory);
+            if (!exists)
+            {
+                System.IO.Directory.CreateDirectory(newDirectory);
+            }
+
+            return newFileName;
+        }
+
+        private void tagButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true)
+            {
+                tagButton.Enabled = false;
+                cancelTagButton.Enabled = true;
+                images = getImagesInFolders(folderPaths);
+                progressBar1.Visible = true;
+                progressBar1.Value = 0;
+                folderListText.Text = "Tagging Images...\n";
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void cancelTagButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation)
+            {
+                tagButton.Enabled = true;
+                cancelTagButton.Enabled = false;
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            int counter = 0;
+            foreach (String fileName in images)
+            {
+                counter++;
+                worker.ReportProgress((int)(((float)counter / images.Count) * 100), counter);
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                Bitmap newImage = tagImage(Image.FromFile(fileName));
+                ImageFormat imgFormat = ImageFormat.Jpeg;
+
+                if (fileName.Substring(fileName.LastIndexOf('.')) == ".png")
+                {
+                    imgFormat = ImageFormat.Png;
+                }
+                newImage.Save(getNewFileName(fileName), imgFormat);
+                newImage.Dispose();
+                
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                folderListText.Text = "Cancelled";
+            }
+            else if (e.Error != null)
+            {
+                folderListText.Text = "Error: " + e.Error.Message;
+            }
+            else
+            {
+                folderListText.Text = "Done!";
+                SystemSounds.Beep.Play();
+                if (openFolderCheckBox.Checked)
+                {
+                    foreach (String folderPath in folderPaths)
+                    {
+                        Process.Start(@"" + folderPath + "\\Tagged");
+                    }
+                }
+                
+                
+            }
+            progressBar1.Value = 0;
+            folderPaths.Clear();
+            images.Clear();
+            cancelTagButton.Enabled = false;
+            updateList();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            folderListText.Text = "Tagging image " + (int)e.UserState + "/" + images.Count + "\n";
+        }
+
+        private void LogoPreview_Click(object sender, EventArgs e)
+        {
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+            dialog.Filter = "Image files (*.jpg;*.png;*.jpeg)|*.jpg;*.png;*.jpeg";
+            dialog.FilterIndex = 0;
+            //dialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                logoPath = dialog.FileName;
+                LogoPreview.ImageLocation = logoPath;
+            }
+        }
+    }
+}
